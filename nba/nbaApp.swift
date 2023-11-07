b9d624966fe5083e3a84352995e1b29ceed82e1f
ThinkinLion <spearhead0427@gmail.com>
2023-11-07 19:02:31 +0900

    init!!

---
+++ nba/nbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbaApp.swift
+//  nba
+//
+//  Created by 1100690 on 2023/11/07.
+//
+
+import SwiftUI
+
+@main
+struct nbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
