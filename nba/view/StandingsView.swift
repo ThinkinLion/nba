faf6932a233580ca45cbeeaf3e4f6983e65d820b
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2023-11-12 09:33:24 +0900

    asset

---
+++ nba/view/StandingsView.swift
@@ -0,0 +1,20 @@
+//
+//  StandingsView.swift
+//  nba
+//
+//  Created by 1100690 on 2023/11/11.
+//
+
+import SwiftUI
+
+struct StandingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StandingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StandingsView()
+    }
+}
