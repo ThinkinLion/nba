28105eaa81a68d9c8f82e85178f82a9420147cc2
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2023-11-20 08:47:24 +0900

    VerticalLineView

---
+++ nba/view/LineView.swift
@@ -0,0 +1,26 @@
+//
+//  LineView.swift
+//  nba
+//
+//  Created by 1100690 on 2023/11/19.
+//
+
+import SwiftUI
+
+struct VerticalLineView: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: rect.height))
+        return path
+    }
+}
+
+struct HorizontalLineView: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        return path
+    }
+}
