a8612fad3bf072af3598a51e43470e94183e9917
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2023-12-17 12:00:17 +0900

    playerView init with playerId & teamId

--- nba/view/PlayerView.swift
+++ nba/view/PlayerView.swift
@@ -8,32 +8,33 @@
 import SwiftUI
 
 struct PlayerView: View {
-    @StateObject var playerViewModel = PlayerViewModel()
-    //TODO: common한 player viwemodel이 와야
-    var viewModel: SeasonLeaderViewModel
+    @StateObject var viewModel = PlayerViewModel()
+    let playerId: String
+    let teamId: String
     
     var body: some View {
+        let player = PlayerSummaryViewModel(player: viewModel.player)
+        
         ScrollView(.vertical) {
-            let player = PlayerSummaryViewModel(player: playerViewModel.player)
             ZStack(alignment: Alignment(horizontal: .trailing, vertical: .center)) {
-                AsyncImage(url: URL(string: viewModel.imageUrl)) { image in
+                AsyncImage(url: URL(string: playerId.imageUrl)) { image in
                     image.resizable()
                 } placeholder: {}
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 180, height: 180, alignment: .bottom)
-                .padding(.leading, 180)
+                .padding(.leading, 200)
                 .padding(.bottom, -55)
                 .zIndex(0)
                 
                 VStack(alignment: .leading) {
-                    Image(viewModel.teamTriCode)
+                    Image(teamId.teamIdToTriCode)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 50, height: 50)
                         .scaleEffect(1.2)
                         .clipped()
                     
-                    Text(viewModel.upperCasedName)
+                    Text(player.upperCasedName)
                     .foregroundColor(.white)
                     .font(.title)
                     .fontWeight(.bold)
@@ -42,7 +43,7 @@ struct PlayerView: View {
                     
                     Text(player.jerseyAndPosition)
                         .foregroundColor(.gray)
-                        .font(.caption)
+                        .font(.callout)
                 }
                 .frame(width: 160)
                 .padding(.trailing, 200)
@@ -52,30 +53,91 @@ struct PlayerView: View {
             .frame(maxWidth: .infinity)
             .padding(.top, 15)
             .padding(.horizontal, 15)
-            .background(Color(viewModel.light))
+            .background(Color(teamId.light))
         
             Text("")
                 .frame(maxWidth: .infinity)
                 .frame(height: 20)
                 .background {
                     CustomCorner(corners: [.topLeft, ], radius: 20)
-                        .fill(Color(viewModel.dark))
+                        .fill(Color(teamId.dark))
                         .ignoresSafeArea()
                 }
                 .padding(.top, -19)
             
             summaryView(player: player)
+            
+            BannerView(adUnitId: .playerView, paddingTop: 10)
+            
+            rosterView(roster: viewModel.roster)
         }
-        .background(Color(viewModel.dark))
+        .background(Color(teamId.dark))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .onAppear() {
-            playerViewModel.fetchPlayer(documentId: viewModel.playerId)
+            viewModel.fetchPlayer(documentId: playerId)
+            viewModel.fetchRoster(teamId: teamId)
         }
         .ignoresSafeArea()
     }
 }
 
 extension PlayerView {
+    @ViewBuilder
+    func rosterItemView(viewModel: PlayerSummaryViewModel) -> some View {
+        VStack {
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .center)) {
+                AsyncImage(url: URL(string: viewModel.imageUrl)) { image in
+                    image.resizable()
+                } placeholder: {}
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 80, height: 80, alignment: .bottom)
+                    .padding(.trailing, 20)
+                    .zIndex(1)
+                
+                Text(viewModel.jersey)
+                    .frame(width: 30)
+                    .padding(.trailing, 90)
+                    .padding(.bottom, 30)
+                    .foregroundColor(.white.opacity(0.8))
+                    .font(.callout)
+                    .lineLimit(1)
+                    .fontWeight(.semibold)
+                    .zIndex(0)
+            }
+            
+            Text(viewModel.lastName)
+                .foregroundColor(.white.opacity(0.8))
+                .font(.callout)
+                .fontWeight(.bold)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+        }
+        .frame(width: 120)
+    }
+    
+    @ViewBuilder
+    func rosterView(roster: [PlayerModel]) -> some View {
+        VStack(alignment: .leading) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(roster, id: \.self) { player in
+                        let viewModel = PlayerSummaryViewModel(player: player)
+                        NavigationLink(destination: PlayerView(playerId: viewModel.playerId, teamId: viewModel.teamId)) {
+                            rosterItemView(viewModel: viewModel)
+                        }
+                    }
+                }
+                .frame(height: 120)
+                .padding(.leading, 10)
+            }
+            
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 20)
+        .padding(7)
+        .padding(.bottom, 30)
+    }
+    
     @ViewBuilder
     func summaryView(player: PlayerSummaryViewModel) -> some View {
         statsSummaryView(player: player)
@@ -136,7 +198,7 @@ extension PlayerView {
                 .fontWeight(.semibold)
                 .padding(.trailing, 20)
         }
-        .frame(height: 35)
+        .frame(height: 30)
         .frame(maxWidth: .infinity)
     }
     
