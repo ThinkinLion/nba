9c249e6b7cbc091915c2c59e8a7ad29ed5dd5fbd
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2023-12-29 11:26:40 +0900

    BoxScore model

--- nba/model/GamesModel.swift
+++ nba/model/GamesModel.swift
@@ -16,6 +16,7 @@ struct GamesModel: Codable {
 struct HomeAway: Codable, Hashable {
     let home: Game
     let away: Game
+    let leaders: [BoxScore]? //여기에도 모아봤음, 어떤게 쓰일지 몰라서
 }
 
 extension HomeAway {
@@ -32,8 +33,10 @@ struct Game: Identifiable, Codable, Hashable {
     var id = UUID()
     let teamId: String
     let teamCode: String
-    let record: String
+    let record: String?
     let score: String?
+    let leader: BoxScore?
+    let boxscore: [BoxScore]?
 }
 
 extension Game {
@@ -44,4 +47,72 @@ extension Game {
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
+}
+
+struct BoxScore: Codable {
+    var id = UUID()
+    let playerId: String?
+    let firstName: String?
+    let lastName: String?
+    let teamTriCode: String?
+    let jersey: String?
+    let position: String?
+    let pts: String?
+    let reb: String?
+    let ast: String? //여기까지 leader에만 있는 정보
+    
+    /*
+     파이썬
+     player_data = {
+                     "id": str(uuid.uuid4()),
+                     "playerId": player_id,
+                     "title": title,
+                     "firstName": player_firstName,
+                     "lastName": player_lastName,
+                     "positon": position,
+                     "min": min,
+                     "fgm": fgm,
+                     "fga": fga,
+                     "fgp": fgp,
+                     "tpm": tpm,
+                     "tpa": tpa,
+                     "tpp": tpp,
+                     "ftm": ftm,
+                     "fta": fta,
+                     "ftp": ftp,
+                     "oreb": oreb,
+                     "dreb": dreb,
+                     "reb": reb,
+                     "ast": ast,
+                     "stl": stl,
+                     "blk": blk,
+                     "to": to,
+                     "pf": pf,
+                     "pts": pts,
+                     "pm": pm,
+                 }
+     */
+    let title: String? //TOTALS용
+    let min: String?
+    let fgm: String?
+    let fga: String?
+    let fgp: String?
+    
+    let tpm: String?
+    let tpa: String?
+    let tpp: String?
+    
+    let ftm: String?
+    let fta: String?
+    let ftp: String?
+    
+    let oreb: String?
+    let dreb: String?
+    
+    let stl: String?
+    let blk: String?
+    
+    let to: String?
+    let pf: String?
+    let pm: String?
 }
