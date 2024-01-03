75331778d8212646b7ccf61cc3d43ec40b6c4ea2
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2024-01-03 20:27:16 +0900

    ViewModifiers.swift

---
+++ nba/common/ViewModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModifier.swift
+//  nba
+//
+//  Created by 1100690 on 1/3/24.
+//
+
+import SwiftUI
+
+struct ViewModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifier()
+}
