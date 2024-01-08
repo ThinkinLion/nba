e85dfe12e6bb69ef5e5b1bcf5dae14074afc2b59
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2024-01-08 17:49:08 +0900

    ObservableScrollView

---
+++ nba/view/common/ObservableScrollView.swift
@@ -0,0 +1,44 @@
+//
+//  ObservableScrollView.swift
+//  nba
+//
+//  Created by 1100690 on 1/8/24.
+//
+
+import SwiftUI
+
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue = CGFloat.zero
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value += nextValue()
+    }
+}
+
+struct ObservableScrollView<Content>: View where Content : View {
+    @Namespace var scrollSpace
+    @Binding var scrollOffset: CGFloat
+    let content: () -> Content
+    
+    init(scrollOffset: Binding<CGFloat>,
+         @ViewBuilder content: @escaping () -> Content) {
+        _scrollOffset = scrollOffset
+        self.content = content
+    }
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            content()
+                .background(GeometryReader { geo in
+                    let offset = -geo.frame(in: .named(scrollSpace)).minY
+                    Color.clear
+                        .preference(key: ScrollViewOffsetPreferenceKey.self,
+                                    value: offset)
+                })
+        }
+        .coordinateSpace(name: scrollSpace)
+        .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+            scrollOffset = value
+        }
+    }
+}
