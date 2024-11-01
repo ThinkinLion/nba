3dc463ec79fe541771ba67064b18d0271ccde7ef
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2024-11-01 20:55:33 +0900

    version 2.20

--- nba/nbaApp.swift
+++ nba/nbaApp.swift
@@ -11,28 +11,28 @@ import Firebase
 
 @main
 struct nbaApp: App {
-    init() {
-        if !Auth.isDeveloper() {
-            print("isDeveloper not")
-        }
-        FirebaseApp.configure()
-        
-        //admob
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        if Auth.isDeveloper() {
-            if Auth.UUID() == "F527E50F-C470-4C48-B72A-FF632C1995BA" {
-                GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "c6280a9bf7bdadc3cfa7b8067bd4446c" ]
-            } else {
-                GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "9c8f15e14cb4f8190750ea6d7b559c4d" ]
-            }
-        }
+  init() {
+    if !Auth.isDeveloper() {
+      print("isDeveloper not")
     }
+    FirebaseApp.configure()
     
-    var body: some Scene {
-        WindowGroup {
-            NavigationView {
-                StandingsView()
-            }
-        }
+    //admob
+    GADMobileAds.sharedInstance().start(completionHandler: nil)
+    if Auth.isDeveloper() {
+      if Auth.UUID() == "F527E50F-C470-4C48-B72A-FF632C1995BA" {
+        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "c6280a9bf7bdadc3cfa7b8067bd4446c" ]
+      } else {
+        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "9c8f15e14cb4f8190750ea6d7b559c4d" ]
+      }
     }
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        StandingsView()
+      }
+    }
+  }
 }
