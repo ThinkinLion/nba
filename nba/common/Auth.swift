870c70bec24fbec1fef607779fbfda9d2f8abe37
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2024-12-09 15:59:45 +0900

    new iphone

--- nba/common/Auth.swift
+++ nba/common/Auth.swift
@@ -13,9 +13,10 @@ class Auth {
         let uuid = UUID()
         if "504E4735-9B0E-4C2A-BF5E-A0C3C17477FE" == uuid ||
             "00E3BBC9-58CE-425B-8E0E-D2B48278936A" == uuid ||
-            "CC857344-35A5-4C63-B5E5-03868B9797CF" == uuid || //iPhone 14 Pro max
             "F527E50F-C470-4C48-B72A-FF632C1995BA" == uuid || //iPhone 15 Pro
-            "8F655830-FAA0-4189-8B77-A52434955169" == uuid || //iPhone 15 Pro 
+            "8F655830-FAA0-4189-8B77-A52434955169" == uuid || //iPhone 15 Pro
+            "CC857344-35A5-4C63-B5E5-03868B9797CF" == uuid || //iPhone 14 Pro max
+            "8C8AE870-C801-43F6-9333-C679D7E6B47E" == uuid || //iPhone 16 Pro max
             Platform.isSimulator {
             return true
         } else {
