c0d509582047c145e7c913959ccb73b44773fa1a
이재일(Jaeil Lee)/App개발팀/11ST <spearhead@sk.com>
2025-11-17 20:02:00 +0900

    requestConfiguration.testDeviceIdentifiers

--- nba/nbaApp.swift
+++ nba/nbaApp.swift
@@ -20,11 +20,7 @@ struct nbaApp: App {
     //admob
     GADMobileAds.sharedInstance().start(completionHandler: nil)
     if Auth.isDeveloper() {
-      if Auth.UUID() == "F527E50F-C470-4C48-B72A-FF632C1995BA" {
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "c6280a9bf7bdadc3cfa7b8067bd4446c" ]
-      } else {
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "9c8f15e14cb4f8190750ea6d7b559c4d" ]
-      }
+//      GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "fa144699d13077c7e0b5d8c2d008e311" ]
     }
   }
   
